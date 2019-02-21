c3c9be5438386fe20b1474959bd680da25a64e0a
danBLA <danBLA@users.noreply.github.com>
2019-02-21 01:09:14 +0100

    Prepender plugin "AppenderSkipper" to skip appender plugins

--- fuglu/src/fuglu/plugins/p_skipper.py
+++ fuglu/src/fuglu/plugins/p_skipper.py
@@ -83,3 +83,68 @@ e.g. put this in /etc/fuglu/skipplugins.regex:
         filterfile = self.config.get(self.section, 'filterfile')
         filter = SuspectFilter(filterfile)
         return filter.lint()
+
+class AppenderSkipper(PrependerPlugin):
+
+    """Skips appende rplugins based on standard filter file
+This can be used for example to skip spam filters on outgoing messages.
+e.g. put this in /etc/fuglu/skipplugins.regex:
+
+@incomingport    1099    SAPlugin
+"""
+
+    def __init__(self, config, section=None):
+        PrependerPlugin.__init__(self, config, section)
+        self.filter = None
+        self.requiredvars = {
+            'filterfile': {
+                'default': '/etc/fuglu/skipplugins.regex',
+            }
+        }
+        self.logger = self._logger()
+
+    def __str__(self):
+        return "Appender Plugin Skipper"
+
+    def appenderlist(self, suspect, pluginlist):
+        """Removes scannerplugins based on filter file"""
+        if not self._initfilter():
+            return None
+
+        args = self.filter.get_args(suspect)
+        # each arg should be a comma separated list of classnames to skip
+
+        skippluginlist = []
+
+        for arg in args:
+            skippluginlist.extend(arg.split(','))
+
+        listcopy = pluginlist[:]
+        for plug in pluginlist:
+            name = plug.__class__.__name__
+            if name in skippluginlist:
+                listcopy.remove(plug)
+        return listcopy
+
+    def _initfilter(self):
+        if self.filter is not None:
+            return True
+
+        filename = self.config.get(self.section, 'filterfile')
+        if filename is None or filename == "":
+            return False
+
+        if not os.path.exists(filename):
+            self.logger.error('Filterfile not found for skipper: %s' % filename)
+            return False
+
+        self.filter = SuspectFilter(filename)
+        return True
+
+    def lint(self):
+        return self.check_config() and self.lint_filter()
+
+    def lint_filter(self):
+        filterfile = self.config.get(self.section, 'filterfile')
+        filter = SuspectFilter(filterfile)
+        return filter.lint()
